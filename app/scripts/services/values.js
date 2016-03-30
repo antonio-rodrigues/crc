938b7c0f8f78e1224cb52aafb95742ed9ddfb8a9
Antonio Rodrigues <Antonio Rodrigues>
2016-03-30 23:07:18 +0100

    +offline.js +push-notifications (wip)

--- app/scripts/services/values.js
+++ app/scripts/services/values.js
@@ -3,7 +3,9 @@ angular.module('CrcApp')
     title: 'CRC – Central de Risco de Crédito',
     version: '0.1.8',
     api: {
-      url: 'http://localhost:8080'
+      url: 'http://localhost:8080',
+      googleCloudKey: 'AIzaSyCp4id0AWX6GJWzXYHFuu3feBiiM7KXGc8',
+      googleProjectId: 'api-project-147472434866'
     },
     filemap: {
       DEV: 'Novos Devedores',
