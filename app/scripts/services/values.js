48fa47b01aa3340d856aced713a48417b9b6dac5
antonio-rodrigues <sugar.web.design@gmail.com>
2016-04-01 17:30:20 +0100

    README tweaks

--- app/scripts/services/values.js
+++ app/scripts/services/values.js
@@ -2,7 +2,7 @@
 angular.module('CrcApp')
   .constant('appSettings', {
     title: 'CRC – Central de Risco de Crédito',
-    version: '0.1.16',
+    version: '0.1.17',
     api: {
       url: 'http://localhost:8080',
       googleCloudKey: 'AIzaSyCp4id0AWX6GJWzXYHFuu3feBiiM7KXGc8',
