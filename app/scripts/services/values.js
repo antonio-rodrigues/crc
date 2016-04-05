a3dfb2da3106a3382928f62dee9bad8883e94e6b
antonio-rodrigues <sugar.web.design@gmail.com>
2016-04-05 16:31:29 +0100

    added selenium webdriver.io (wip)

--- app/scripts/services/values.js
+++ app/scripts/services/values.js
@@ -2,7 +2,7 @@
 angular.module('CrcApp')
   .constant('appSettings', {
     title: 'CRC – Central de Risco de Crédito',
-    version: '0.1.20-e2e',
+    version: '0.1.21-e2e',
     api: {
       url: 'http://localhost:8080',
       googleCloudKey: 'AIzaSyCp4id0AWX6GJWzXYHFuu3feBiiM7KXGc8',
