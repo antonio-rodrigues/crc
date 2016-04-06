e99db997ec7dbd344b46f3b767184cdc3597b031
antonio-rodrigues <antonio.rodrigues@gmail.com>
2016-04-06 13:05:29 +0100

    merge from repo-h

--- app/scripts/controllers/filesController.js
+++ app/scripts/controllers/filesController.js
@@ -1,10 +1,10 @@
 (function () {
   // 'use strict';
 
-  var filesController = function($scope, $routeParams, $log, filesFactory, appSettings) {
-    
+  var filesController = function ($scope, $routeParams, $log, filesFactory, appSettings) {
+
     var vm = this;
-    
+
     var fileType = $routeParams.fileType || 'DEV';
     var fileId = $routeParams.fileId || null;
     console.log('fileType: ' + fileType);
@@ -17,26 +17,26 @@
       title: appSettings.filemap[fileType.toUpperCase()],
       itemsPerPage: 5
     };
-    
+
     vm.xmlfiles = [];
-    
+
     function init() {
       //Favor then() insteadOf deprecated success()
       filesFactory.getFiles(fileType.toUpperCase())
-        .then(function(response) {
+        .then(function (response) {
           vm.xmlfiles = response.data;
-        }, function(data, status, headers, config) {
+        }, function (data, status, headers, config) {
           $log.log(data.error + ' ' + status);
         });
     }
-    
+
     init();
-    
-    $scope.doSort = function(propName) {
+
+    $scope.doSort = function (propName) {
       $scope.sortBy = propName;
       $scope.reverse = !$scope.reverse;
     };
-    
+
   };
 
   // TODO: implement ng-annotate (and save a bunch of typing!)
